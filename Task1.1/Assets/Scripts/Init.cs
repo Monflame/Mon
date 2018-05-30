7d0cfd5bbf96493690f991df12468c631873e466
Monflame <monflame@gmail.com>
2018-05-31 00:27:47 +0300

    a new repository

---
+++ Task1.1/Assets/Scripts/Init.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Init : MonoBehaviour {
+
+	public Canvas roomObj;
+
+	void Start ()
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			Instantiate(roomObj, new Vector3(0,i+i,95), Quaternion.identity);
+		}
+    }
+}
---
+++ Task1.1/Assets/Scripts/WindowManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class WindowManager : MonoBehaviour {
+
+	public Dropdown dropdown;
+	List<string> dropOptions = new List<string> {"Casino", "Bingo"};
+	public Text dropText;
+	public Image room;
+
+	void Start()
+	{
+		/*switch(dropdown.value){
+			case 0:
+				room.gameObject.SetActive(false);
+				Debug.Log("result =" +dropdown.value);
+				break;
+			case 1:
+				room.gameObject.SetActive(true);
+				Debug.Log("result =" +dropdown.value);
+				break;
+		}*/
+		dropdown = GetComponent<Dropdown>();
+		dropdown.ClearOptions();
+		dropdown.AddOptions(dropOptions);
+	}
+	
+		/*switch(dropdown.value){
+			case 0:
+				room.gameObject.SetActive(false);
+				Debug.Log("result =" +dropdown.value);
+				break;
+			case 1:
+				room.gameObject.SetActive(true);
+				Debug.Log("result =" +dropdown.value);
+				break;
+		}*/	
+}
