b5a7ffea26fcb4ac2e700aa88e061d26aae9739d
monflame <monflame@gmail.com>
2018-07-06 18:43:14 +0300

    tried enemy movement with update method/ failed

--- CardGame/Assets/Scripts/EnemyTurn.cs
+++ CardGame/Assets/Scripts/EnemyTurn.cs
@@ -7,14 +7,12 @@ public class EnemyTurn : MonoBehaviour
 	public Vector3 fromPos;
 	public Vector3 toPos;
 	GameManager gameManager;
-	GameObject HandEnemy;
-	GameObject FieldEnemy;
+	public Transform HandEnemy;
+	public Transform FieldEnemy;
 
 	void Awake()
 	{
 		gameManager = FindObjectOfType<GameManager>();
-		HandEnemy = GameObject.Find("HandEnemy");
-		FieldEnemy = GameObject.Find("FieldEnemy");
 		fromPos = HandEnemy.transform.position;
 		toPos = FieldEnemy.transform.position;
 	}
@@ -23,9 +21,20 @@ public class EnemyTurn : MonoBehaviour
 	{
 		for(int i = 0; i < gameManager.enemyList.Count; i = gameManager.enemyList.Count-1)
 		{
-			gameManager.enemyList[i].transform.position = Vector3.Lerp(fromPos, toPos, 100);
+			gameManager.enemyList[i].transform.position = Vector3.Lerp(fromPos, toPos, 0.5f);
 			gameManager.enemyList.Remove(gameManager.enemyList[i]);
-			yield return new WaitForSeconds(1.5f);
+			yield return new WaitForSeconds(1);
 		}
 	}
+
+	/*private void Update()
+	{
+		if(!gameManager.IsPlayerTurn)
+			Move();
+	}
+
+	private void Move()
+	{
+		gameManager.enemyList[1].transform.position = Vector2.MoveTowards(fromPos, toPos, 1 * Time.deltaTime);
+	}*/
 }
